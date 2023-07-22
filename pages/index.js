f20f7521e5bb90cec0042f6b6a4046313356af8c
Mariam Fatima <118051988+mariamffatima@users.noreply.github.com>
2023-07-22 17:46:28 +0500

    side menu

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,43 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
+import {PiCodesandboxLogoLight} from 'react-icons/pi'
+import {GoDatabase} from 'react-icons/go'
+import {SlSettings} from 'react-icons/sl'
+import {RxDashboard} from 'react-icons/rx'
+import {BsWindowSidebar} from 'react-icons/bs'
+import {MdLogout} from 'react-icons/md'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className=''>
-      <title>My Stack</title>
-      <div className='grid grid-cols-2 '>
-      <div className='w-60 h-full relative flex flex-col bg-[#F4C9CB] drop-shadow-md shadow-md shadow-[#0000001C]'>
-        <div className='p-4 px-6 py-4 font-bold text-xl'>
+      <div>
+      <div className='w-1/2 md:w-1/4 lg:w-1/5 h-screen flex flex-col bg-[#F4C9CB] drop-shadow-md shadow-md shadow-[#0000001C]'>
+        <div className='p-4 px-6 py-4 font-bold text-xl flex row hover:cursor-default'>
+          <PiCodesandboxLogoLight className='flex mt-1 m-2 text-2xl'/>
           My Stack
           </div>
-          <ul className='p-2 text-lg text-gray-600'>
-            <li className='px-3 py-2 hover:text-black hover:cursor-pointer hover:scale-100 hover:bg-[#fcfcfc]'>Summer Coding</li>
-            <li className='px-3 py-2 hover:text-black hover:cursor-pointer hover:scale-100 hover:bg-[#fcfcfc]'>Data Science</li>
-            <li className='px-3 py-2 hover:text-black hover:cursor-pointer hover:scale-100 hover:bg-[#fcfcfc]'>Settings</li>
-            <li className='px-3 py-2 hover:text-black hover:cursor-pointer hover:scale-100 hover:bg-[#fcfcfc]'>Courses</li>
+          <div className='flex flex-col'>
+          <ul className='p-2 text-lg text-gray-700 '>
+          <li className='px-3 py-2 hover:text-black hover:cursor-pointer hover:scale-100 hover:bg-[#fcfcfc] flex flex-row'>
+            <RxDashboard className='flex mt-1 m-2 text-xl'/>Courses</li>
+          <li className='px-3 py-2 hover:text-black hover:cursor-pointer hover:scale-100 hover:bg-[#fcfcfc] flex flex-row'>
+             <BsWindowSidebar className='flex mt-1 m-2 text-xl'/>Summer Coding</li>
+          <li className='px-3 py-2 hover:text-black hover:cursor-pointer hover:scale-100 hover:bg-[#fcfcfc] flex flex-row'>
+             <GoDatabase className='flex mt-1 m-2 text-xl'/> Data Science</li>
+          <li className='px-3 py-2 hover:text-black hover:cursor-pointer hover:scale-100 hover:bg-[#fcfcfc] flex flex-row'> 
+             <SlSettings className='flex mt-1 m-2 text-xl'/>Settings</li>
+            
           </ul>
-          <div className='m-4 max-h-fit p-2 px-2 py-4 border align-baseline flex justify-center text-black hover:cursor-pointer hover:scale-100 hover:bg-[#fcfcfc]'>Logout</div>
+          <div className=''>
+          <div className='flex m-4 p-2 px-2 py-4 text-xl border justify-center mt-96 text-black hover:cursor-pointer hover:scale-100 hover:bg-[#fcfcfc]'>
+            <MdLogout className='flex mt-1 m-2 text-2xl'/>Logout</div>
+            </div>
+          </div>
+          </div>
+      <div className='absolute top-0 '>
+        Summer Coding Challenge
       </div>
-      <div></div>
+     
       </div>
-    </main>
   )
 }
