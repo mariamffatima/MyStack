8b5973df610d9f39781fee1a44d920a82b68d6e3
Mariam Fatima <118051988+mariamffatima@users.noreply.github.com>
2023-07-28 14:29:31 +0500

    updates in layout and responsiveness

--- pages/homepage.js
+++ pages/homepage.js
@@ -28,7 +28,7 @@ function SearchBar() {
         type='text'
         value={inputValue}
         onChange={handleChange}
-        className='bg-transparent outline-none justify-start items-center text-sm w-[9.625rem] text-gray-800 placeholder-gray-600'
+        className='bg-[#fcfcfc] outline-none justify-start items-center text-sm w-[9.625rem] text-gray-800 placeholder-gray-600'
         placeholder='Search'
       />
     </div>
@@ -48,13 +48,12 @@ export default function HomePage() {
   };
 
   return (
-    <div>
+<div>
+    <div className='felx h-screen text-black bg-[#fcfcfc]'> 
+        <div className='hidden md:flex'><SideMenu /></div>
+        <div className='md:absolute md:inline-block md:top-0 md:ml-[16rem] flex flex-col'>
 
-      <div className='felx h-screen text-black bg-[#fcfcfc]'> 
-        <SideMenu className='hidden md:flex'/>
-        <div className='w-full h-screen flex flex-col'>
-
-          <div className='h-fit border-b-2 border-gray-400 flex flex-col w-full space-y-4 mb-2 px-5 sm:px-10 md:pt-5 pt-1'>
+          <div className='h-fit border-b-2 border-gray-400 flex flex-col w-full space-y-4 mb-2 px-4 sm:px-10 md:pt-5 pt-1'>
               
              <div className='flex justify-between'>
 
@@ -118,7 +117,7 @@ export default function HomePage() {
               </div>
               </div>
 
-              <div className='flex items-center justify-end space-x-4 md:space-x-8 sm:pe-5 pb-3'>
+              <div className='flex items-center justify-end space-x-3 md:space-x-8  pb-3'>
                 <span><SearchBar className='' /></span>
                 <span className='text-xl md:text-2xl'><BsFillBellFill  /></span>
                 <span className='text-xl md:text-2xl' ><RiSettings5Fill /></span>
@@ -128,10 +127,15 @@ export default function HomePage() {
 
             </div>
 
-            <div className='w-[49.625rem] h-[40.125rem] flex flex-shrink-0  bg-[#FFD3D5] drop-shadow-md shadow-md rounded-lg shadow-gray-400'>
-              <div className='w-[23.0625rem] ml-5 mt-4  h-[38.125rem] flex flex-shrink-0 bg-[#f3f3f3] rounded-lg flex-col'>
-                <div className='flex flex-shrink-0 text-xl text-black font-bold m-2 px-4 '>To Do List</div>
-                <div className='text-black w-[20.59rem] overflow-y-scroll scrollbar-none h-[14.2rem] ml-4 mt-2 py-2 bg-[#ffd3d5] space-y-3 rounded-lg'>
+
+            <div className='px-3 py-3 flex grow space-x-4 m-1 overflow-hidden scrollbar-hidden'>
+
+                <div className='w-[80%] h-fit bg-[#FFD3D5] grid grid-cols-1 xl:grid-cols-2 drop-shadow-md shadow-md xl:space-x-8 p-4 rounded-lg shadow-gray-400'>
+              
+              <div className='w-[100%] ml-2 mt-2 flex flex-shrink-0 bg-[#f3f3f3] rounded-lg flex-col'>
+                <div className='flex flex-shrink-0 text-xl text-black font-bold m-2 px-4 '>
+                    To Do List</div>
+                <div className='text-black w-[90%] h-[40%] overflow-y-scroll scrollbar-none  ml-4 mt-2 py-2 bg-[#ffd3d5] space-y-3 rounded-lg'>
                   <label className='relative px-[1.25rem] py-[0.5rem] flex cursor-pointer bg-[#fcfcfc] ml-4 mr-4  rounded-lg'>
                     <input
                       type='checkbox'
@@ -139,30 +143,70 @@ export default function HomePage() {
                     />
                     <span className='px-3 py-[0.1rem] '>Watch 1 design class</span>
                   </label>
-                  {/* Add other to-do items here */}
+                  <label className='relative px-[1.25rem] py-[0.5rem] flex cursor-pointer bg-[#fcfcfc] ml-4 mr-4  rounded-lg'>
+                    <input
+                      type='checkbox'
+                      className=' h-[1.3rem] w-[1.3rem] mt-1 cursor-pointer flex text-center accent-[#e8b4b8] border-gray-600 '
+                    />
+                    <span className='px-3 py-[0.1rem] '>Attend seminar</span>
+                  </label>
+                  <label className='relative px-[1.25rem] py-[0.5rem] flex cursor-pointer bg-[#fcfcfc] ml-4 mr-4  rounded-lg'>
+                    <input
+                      type='checkbox'
+                      className=' h-[1.3rem] w-[1.3rem] mt-1 cursor-pointer flex text-center accent-[#e8b4b8] border-gray-600 '
+                    />
+                    <span className='px-3 py-[0.1rem] '>Compelete project</span>
+                  </label>
+                  <label className='relative px-[1.25rem] py-[0.5rem] flex cursor-pointer bg-[#fcfcfc] ml-4 mr-4  rounded-lg'>
+                    <input
+                      type='checkbox'
+                      className=' h-[1.3rem] w-[1.3rem] mt-1 cursor-pointer flex text-center accent-[#e8b4b8] border-gray-600 '
+                    />
+                    <span className='px-3 py-[0.1rem] '>Submit Assignment</span>
+                  </label>
+                  <label className='relative px-[1.25rem] py-[0.5rem] flex cursor-pointer bg-[#fcfcfc] ml-4 mr-4  rounded-lg'>
+                    <input
+                      type='checkbox'
+                      className=' h-[1.3rem] w-[1.3rem] mt-1 cursor-pointer flex text-center accent-[#e8b4b8] border-gray-600 '
+                    />
+                    <span className='px-3 py-[0.1rem] '>Watch series</span>
+                  </label>
                 </div>
-                <div className='flex flex-shrink-0 text-xl text-black font-bold m-2 px-4 '>Inbox</div>
-                <div className='text-black w-[20.59rem] overflow-y-scroll scrollbar-none h-[14.2rem] ml-4 mt-2 py-2 bg-[#ffd3d5] space-y-3 rounded-lg'>
+                <div className='flex flex-shrink-0 text-xl text-black font-bold m-2 px-4 '>
+                    Inbox</div>
+                <div className='text-black w-[90%] h-[40%] overflow-y-scroll scrollbar-none  ml-4 mt-2 mb-2  py-2 bg-[#ffd3d5] space-y-3 rounded-lg'>
                   <label className='relative px-[1.25rem] py-[0.5rem] flex cursor-pointer bg-[#fcfcfc] ml-4 mr-4  rounded-lg'>
                     <input
                       type='checkbox'
                       className=' h-[1.3rem] w-[1.3rem] mt-1 cursor-pointer flex text-center accent-[#e8b4b8] border-gray-600 '
                     />
-                    <span className='px-3 py-[0.1rem] '>Watch 1 design class</span>
+                    <span className='px-3 py-[0.1rem] '>Discuss details of project</span>
                   </label>
-                  {/* Add other inbox items here */}
+                  <label className='relative px-[1.25rem] py-[0.5rem] flex cursor-pointer bg-[#fcfcfc] ml-4 mr-4  rounded-lg'>
+                    <input
+                      type='checkbox'
+                      className=' h-[1.3rem] w-[1.3rem] mt-1 cursor-pointer flex text-center accent-[#e8b4b8] border-gray-600 '
+                    />
+                    <span className='px-3 py-[0.1rem] '>Attend workshop</span>
+                  </label>
+                  <label className='relative px-[1.25rem] py-[0.5rem] flex cursor-pointer bg-[#fcfcfc] ml-4 mr-4  rounded-lg'>
+                    <input
+                      type='checkbox'
+                      className=' h-[1.3rem] w-[1.3rem] mt-1 cursor-pointer flex text-center accent-[#e8b4b8] border-gray-600 '
+                    />
+                    <span className='px-3 py-[0.1rem] '>Invitation for code camp</span>
+                  </label>
                 </div>
               </div>
 
-              <div className='w-[23.0625rem] ml-4 mt-4 h-[38.125rem] flex flex-shrink-0 flex-col rounded-lg bg-[#f3f3f3]'>
+              <div className='w-3.5/4 ml-2 mt-2  flex flex-col overflow-scroll scrollbar-none rounded-lg bg-[#f3f3f3]'>
                 <span className=''>
                   <textarea
                     className=' flex justify-center focus:border-none
-            w-[20.59rem] h-28 ml-4 mt-4 p-3 scrollbar-hide text-black border-gray-600 bg-[#fcfcfc] rounded-lg '
+                    w-[90%] h-[70%] ml-4 mt-4 p-3 overflow-scroll scrollbar-none text-black border-gray-600 bg-[#fcfcfc] rounded-lg '
                     placeholder='Add description'
                   ></textarea>
                 </span>
-
                 <div className='flex flex-row px-[1.25rem] py-[0.5rem] font-bold'>
                   <span className='text-lg mr-2'>Priority</span>
                   <div className='flex flex-row text-md space-x-2'>
@@ -212,29 +256,35 @@ export default function HomePage() {
                     type='text'
                     placeholder='Add tags like exercise, work, etc. '
                     className='bg-[#fcfcfc] ml-4 w-[20.5rem] h-[3.19rem] px-3 py-2 rounded-lg '
-                  />{' '}
+                  />
                 </span>
+                <div className='text-black p-3 flex justify-end m-2'>
+                    <button className='  gap-[0.625rem] p-1.5 border-2 rounded-lg border-gray-400 hover:focus:bg-[#e8b4b8] focus:text-black bg-[#f3f3f3] flex justify-center items-center '>
+                        Save Task</button>
+                 </div>
               </div>
+                </div>
+
+                <div className='w-[30%] flex-col pb-8 h-fit rounded-lg bg-[#f3f3f3]'>
+                  <span className='flex justify-center text-black px-4 mt-4 font-bold text-xl'>Qoute</span>
+                  <div className='m-2 py-2 w-[95%]  bg-[#fcfcfc] rounded-lg'>
+                    <div className='px-4 text-lg flex justify-end text-black'>
+                      <SlOptions />
+                    </div>
+                     <div className='ml-2 mr-2 px-1 py-1 overflow-y-scroll scrollbar-none text-black flex justify-center '>
+                       We have three roles here on earth: to learn, to love, and to live. When we stop learning, we start to
+                      stagnate and die. When we stop loving, we lose our sense of purpose and become self-centered. When we
+                       limit our living, we deny the world the benefits of our talents.
+                       <br></br>
+                      Jim Cathcart, The Acorn Principle
+                   </div>
+                  </div>
+                 </div>
+
             </div>
 
-            <div className='absolute ml-[52rem] flex flex-shrink-0 flex-col w-[22.43rem] mt-[5.5rem] h-[40.125rem] rounded-lg bg-[#f3f3f3]'>
-              <span className='flex justify-center text-black px-4 mt-4 font-bold text-xl'>Qoute</span>
-              <span className='w-[20.59rem] h-[14.2rem] ml-4 mt-4 py-2 bg-[#fcfcfc] rounded-lg'>
-                <div className='px-4 text-lg flex justify-end text-black'>
-                  <SlOptions />
-                </div>
-                <div className='ml-2 mr-2 px-1 py-1 text-black flex justify-center'>
-                  We have three roles here on earth: to learn, to love, and to live. When we stop learning, we start to
-                  stagnate and die. When we stop loving, we lose our sense of purpose and become self-centered. When we
-                  limit our living, we deny the world the benefits of our talents.
-                  <br></br>
-                  Jim Cathcart, The Acorn Principle
-                </div>
-              </span>
-            </div>
-          
         </div>
-      </div>
     </div>
+</div>
   );
                 }
